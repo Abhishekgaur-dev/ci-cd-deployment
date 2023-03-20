31079898c9405b18fe336efcb75fd4073c33b5fd
RajRonal <ronal.raj@remotestate.com>
2023-03-20 15:30:01 +0530

    fixed prod

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>this is prod check.</p>
+        <p>this is prod check again.</p>
         <a
           className='App-link'
           href='https://reactjs.org'
